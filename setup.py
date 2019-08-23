07c5295bf1b20c244884beb1d465218b114dfa72
Martin Denk <denk.martin1990@gmail.com>
2019-08-23 13:50:40 +0200

    Added ULR to SETUP File

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
    name='ToOptix',
    version='1.0',
    description='OptimizationModule for Topology',
-   author='Martin Denk',
-   author_email='denk.martin1990@gmail.com',
+   author='Martin Denk https://orcid.org/0000-0002-0204-3608',
+   url='https://github.com/DMST1990/ToOptix',
    packages=['ToOptix', 'ToOptix.FEMPy', 'ToOptix.FEMPy.Geometry'],  
    install_requires=['numpy'], #external packages as dependencies alternative --> Trimesh and Mayavi if true
    )
